15020c7db6f72955803f65f166889cc21207ce5f
Lu Ye Zhan <lu.ye.external@axa.com>
2020-03-03 17:17:51 +0100

    using services

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,16 +1,22 @@
 import { Recipe } from "./recipe.model";
+import { Ingredient } from "../shared/ingredient.model";
+import { EventEmitter } from "@angular/core";
 
 export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
+
   private recipes: Recipe[] = [
     new Recipe(
       "A Test Recipe",
       "This is simply a test",
-      "https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg"
+      "https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg",
+      [new Ingredient("Meat", 1), new Ingredient("french frieds", 20)]
     ),
     new Recipe(
       "Another Test Recipe",
       "This is simply a test",
-      "https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg"
+      "https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg",
+      [new Ingredient("Buns", 2), new Ingredient("Meat", 1)]
     )
   ];
 
