6846e3ffd9207af3b113c0b7f43dadac5ed205d0
Lu Ye Zhan <lu.ye.external@axa.com>
2020-04-20 09:52:16 +0200

    configuring route parameters

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -19,13 +19,17 @@ export class RecipeService {
       "This is simply a test",
       "https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg",
       [new Ingredient("Buns", 2), new Ingredient("Meat", 1)]
-    )
+    ),
   ];
 
   constructor(private slService: ShoppingListService) {}
 
   getRecipes() {
     return this.recipes.slice();
+  }
+
+  getRecipe(index: number) {
+    return this.recipes.slice()[index];
   }
 
   addIngredientsToShoopingList(ingredients: Ingredient[]) {
